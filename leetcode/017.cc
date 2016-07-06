b3caea269b9c809d2f288836bfa15f1154af67a4
alex4814 <alex4814@zju.edu.cn>
2016-07-06 19:22:22 +0800

    Leetcode 017

--- leetcode/017.cc
+++ leetcode/017.cc
@@ -7,11 +7,34 @@ using namespace std;
 class Solution {
 public:
   vector<string> letterCombinations(string digits) {
-    
+    vector<string> ret;
+    string curr;
+    dfs(digits, 0, curr, ret);
+    return ret;
+  }
+  void dfs(string digits, int i, string &curr, vector<string> &ret) {
+    static const vector<string> mappings = {
+      "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+    if (i == digits.size()) {
+      if (i > 0) {
+        ret.push_back(curr);
+      }
+      return;
+    }
+    for (const char c: mappings[digits[i] - '0']) {
+      curr.push_back(c);
+      dfs(digits, i + 1, curr, ret);
+      curr.pop_back();
+    }
   }
 };
 
 int main() {
-
+  Solution s;
+  auto x = s.letterCombinations("23");
+  for (auto str: x) {
+    cout << str << endl;
+  }
   return 0;
 }
