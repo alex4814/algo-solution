75420dde61976001cbea5dc1a77a7564347ebfc6
Ruisheng Fu <alex4814.fu@gmail.com>
2017-08-24 00:04:27 +0800

    Everyday euler 08/23

---
+++ project-euler/009.py
@@ -0,0 +1,7 @@
+for a in xrange(0, 1000):
+	for b in xrange(a+1, 1000):
+		c = 1000 - a - b
+		if c <= b:
+			break
+		if a * a + b * b == c * c:
+			print a * b * c
---
+++ project-euler/010.py
@@ -0,0 +1,18 @@
+from array import array
+
+MAX = 2000000
+is_composite = {}
+primes = array('l')
+
+for n in xrange(2, MAX):
+	if not is_composite.setdefault(n, False):
+		primes.append(n)
+	for p in primes:
+		m = p * n
+		if m >= MAX:
+			break
+		is_composite[m] = True
+		if n % p == 0:
+			break
+
+print sum(primes)
